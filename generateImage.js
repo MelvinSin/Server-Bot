0f9e465d316f80bd31af82e96477f6efd1d6995e
C6m0 <melvin.ciglaric@gmail.com>
2023-02-23 19:33:46 +0100

    welcome message (untested)

--- generateImage.js
+++ generateImage.js
@@ -47,7 +47,23 @@ const generateImage = async (member) => {
     ctx.drawImage(avimg, av.x, av.y)
     ctx.restore()
 
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png")
+    //write in text
+    ctx.fillStyle = "white"
+    ctx.textAlign = "center"
+
+    //draw in welcome
+    ctx.font = "50px Roboto"
+    ctx.fillText("Welcome", dim.width/2, dim.margin + 70)
+
+    //draw in username
+    ctx.font = "60px Roboto"
+    ctx.fillText(username + discrim, dim.width/2, dim.height - dim.margin - 125)
+
+    //draw in to the server
+    ctx.font = "40px Roboto"
+    ctx.fillText("to the server", dim.width/2, dim.height - dim.margin - 50)
+
+    const attachment = new Discord.AttachmentBuilder(canvas.toBuffer(), "welcome.png")
     return attachment
 }
 
